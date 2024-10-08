c5301d3bc9164beccb5062c961f6a183d814802e
KiSki-Dev <106085521+KiSki-Dev@users.noreply.github.com>
2024-10-08 22:35:16 +0200

    Pre-Release 0.2 : About and Help Section

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #from pynput import keyboard
 import os
 import time
+import webbrowser
 
-mainMenuString = "[1] Start\n[2] Stop Shutdown\n[3] Settings & Help\n[4] Exit"
+mainMenuString = "[1] Start\n[2] Stop Shutdown\n[3] About & Help\n[4] Exit"
 headerString = "AUTO OFFLINE CLIENT\n"
+version = "0.2"
 
 def mainMenu():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -19,7 +21,7 @@ def mainMenu():
         elif opt == 2:
             stopShutdown()
         elif opt == 3:
-            print("3")
+            aboutHelp()
         elif opt == 4:
             print("\n\nThanks for using! :)")
             print("Exiting...")
@@ -39,7 +41,7 @@ def start():
 
     if os.name == 'nt':
         print(headerString)
-        print("Enter amount with s for seconds, m for minutes, h for hours, and d for days.\nExample: 42m = 42 minutes\n")
+        print("\nEnter amount with s for seconds, m for minutes, h for hours, and d for days.\nExample: 42m = 42 minutes\n")
         opt = input("Pick amount: ")
 
         if opt.isdigit() == False: # If opt does not contain any Format
@@ -129,10 +131,6 @@ def stopShutdown():
 
     mainMenu()
 
-def settingsHelp():
-    print("settingsHelp")
-
-
 def shutdown(amount):
     if os.name == 'nt':
         try:
@@ -175,6 +173,74 @@ def shutdown(amount):
 
     mainMenu()
 
+def aboutHelp():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(headerString)
+    print("[1] About\n[2] Help\n[3] Back")
+
+    opt = input("\nSelect an option: ")
+
+    if opt.isdigit():
+        opt = int(opt)
+        if opt == 1:
+            about()
+        elif opt == 2:
+            help()
+        elif opt == 3:
+            mainMenu()
+        else:
+            print("Invalid option. Please try again.")
+            time.sleep(0.3)
+            aboutHelp()
+    else:
+        print("Invalid option. Please try again.")
+        time.sleep(0.3)
+        aboutHelp()
+
+def about():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(headerString)
+    print("[1] Check for Updates\n[2] GitHub\n[3] Discord Support Server")
+
+    opt = input("\nSelect an option: ")
+
+    if opt.isdigit():
+        opt = int(opt)
+        if opt == 1:
+            checkForUpdate()
+        elif opt == 2:
+            webbrowser.open("https://github.com/KiSki-Dev", new=2)
+            input("\nOpening 'https://github.com/KiSki-Dev'")
+            mainMenu()
+
+        elif opt == 3:
+            webbrowser.open("https://discord.com/invite/cYqpx7dqsn", new=2)
+            input("\nOpening 'https://discord.com/invite/cYqpx7dqsn'")
+            mainMenu()
+
+        else:
+            print("Invalid option. Please try again.")
+            time.sleep(0.3)
+            aboutHelp()
+    else:
+        print("Invalid option. Please try again.")
+        time.sleep(0.3)
+        aboutHelp()
+
+def help():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(headerString)
+    print("You can select your time until shutdown using the following units:\ns = seconds | m = minutes | h = hours | d = days | w = weeks\n\nYou can also use the following quick Parameters.")
+
+    input("\nPress enter to get back to the Main Menu.")
+    mainMenu()
+
+def checkForUpdate():
+    print("updated")
+    # To-Do : use json for Informations
 
 
 if __name__ == "__main__":
